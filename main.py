1569bbadb6642fa0b59a2638f8b4ab13c5e95d8a
Mohammed Abdulai <nurud43@gmail.com>
2024-07-02 19:26:48 -0400

    next card function

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ from tkinter import messagebox
 from random import shuffle, choice, randint
 import pyperclip
 import json
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-# ---------------------------- UI SETUP ------------------------------- #
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
 
+# ---------------------------- UI SETUP ------------------------------- #
+def next_card():
+    current_card = random.choice(to_learn)
+    card = current_card["French"]
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
 
 window = Tk()
 window.title("Flashy")
@@ -16,17 +25,17 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 cross_img = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_img, highlightthickness=0)
+unknown_button = Button(image=cross_img, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 check_mark = PhotoImage(file="images/right.png")
-known_button = Button(image=check_mark,highlightthickness=0)
+known_button = Button(image=check_mark, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
 
 window.mainloop()
